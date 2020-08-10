376ed55b90c7de0ceda180a1eaafde808f890f27
ceci21 <ceci@ODYSSEUS.local>
2020-08-09 22:19:33 -0500

    Updates

---
+++ precache-manifest.f370421a3d29dedabe6c5e727e3c8b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2c35f108b533ce251369200c5930e8",
+    "url": "/color-quiz/index.html"
+  },
+  {
+    "revision": "e1e8b4317aea04f0825d",
+    "url": "/color-quiz/static/css/main.61736f28.chunk.css"
+  },
+  {
+    "revision": "2578aa7e2748db514234",
+    "url": "/color-quiz/static/js/2.1e786ba6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-quiz/static/js/2.1e786ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e8b4317aea04f0825d",
+    "url": "/color-quiz/static/js/main.930f4922.chunk.js"
+  },
+  {
+    "revision": "de6ac0197a6906619943",
+    "url": "/color-quiz/static/js/runtime-main.0c1a1c8d.js"
+  }
+]);
