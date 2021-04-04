bc1198c6e7301a6e16d6b243896ab622a5350276
ceci21 <ceci.goss@gmail.com>
2021-04-04 15:45:03 -0500

    add new Qs

--- src/data.js
+++ src/data.js
@@ -1,166 +1,197 @@
 import {
-    START,
-    QUESTION_TYPE_MULTIPLE,
-    RESULT_RED,
-    RESULT_BLUE,
-    RESULT_YELLOW,
-    RESULT_GREEN,
-    RESULT_BLACK,
-    RESULTS
-} from "./constants";
+  START,
+  QUESTION_TYPE_MULTIPLE,
+  RESULT_RED,
+  RESULT_BLUE,
+  RESULT_YELLOW,
+  RESULT_GREEN,
+  RESULT_BLACK,
+  RESULTS,
+} from './constants';
 
 const DATA = [
-    {
-        // START
-        name: "start",
-        type: START
-    },
-    {
-        // Question one
-        name: "question-one",
-        type: QUESTION_TYPE_MULTIPLE,
-        question: "Which of these fruits do you prefer?",
-        choices: [
-            {
-                // Choice one
-                answer: "Blueberry",
-                result: RESULT_RED,
-                add: 1
-            },
-            {
-                // Choice two
-                answer: "Apple",
-                result: RESULT_BLUE,
-                add: 1
-            },
-            {
-                // Choice three
-                answer: "Banana",
-                result: RESULT_YELLOW,
-                add: 1
-            },
-            {
-                // Choice four
-                answer: "Grape",
-                result: RESULT_GREEN,
-                add: 1
-            },
-            {
-                // Choice five
-                answer: "\"I don't like fruit. It scares me.\"",
-                result: RESULT_BLACK,
-                add: 1
-            }
-        ]
-    },
-    {
-        // Question two, this question doesn't influence the results at all.
-        name: "question-two",
-        type: QUESTION_TYPE_MULTIPLE,
-        question: "What is your name?",
-        choices: [
-            {
-                // Choice one
-                answer: "Joshua",
-                result: 0,
-                add: 0
-            },
-            {
-                // Choice two
-                answer: "Denise",
-                result: 0,
-                add: 0
-            },
-            {
-                // Choice three
-                answer: "Not listed",
-                result: 0,
-                add: 0
-            }
-        ]
-    },
-    {
-        // Question three
-        name: "question-three",
-        type: QUESTION_TYPE_MULTIPLE,
-        question: "How is your day?",
-        choices: [
-            {
-                // Choice one
-                answer: "Awesome",
-                result: RESULT_RED,
-                add: 1
-            },
-            {
-                // Choice two
-                answer: "Good",
-                result: RESULT_BLUE,
-                add: 1
-            },
-            {
-                // Choice three
-                answer: "Decent",
-                result: RESULT_YELLOW,
-                add: 1
-            },
-            {
-                // Choice four
-                answer: "Amiss",
-                result: RESULT_GREEN,
-                add: 1
-            },
-            {
-                // Choice five
-                answer: "Bad",
-                result: RESULT_BLACK,
-                add: 1
-            }
-        ]
-    },
-    {
-        // Question four
-        name: "question-four",
-        type: QUESTION_TYPE_MULTIPLE,
-        question: "Pick a color that describes you: ",
-        choices: [
-            {
-                // Choice one
-                answer: "Red",
-                result: RESULT_BLUE,
-                add: 5
-            },
-            {
-                // Choice two
-                answer: "Blue",
-                result: RESULT_RED,
-                add: 1
-            },
-            {
-                // Choice three
-                answer: "Yellow",
-                result: RESULT_YELLOW,
-                add: 1
-            },
-            {
-                // Choice four
-                answer: "Magenta",
-                result: RESULT_GREEN,
-                add: 1
-            },
-            {
-                // Choice five
-                answer: "Purple",
-                result: RESULT_BLACK,
-                add: 1
-            }
-        ]
-    },
-    {
-        // Results
-        name: "results",
-        type: RESULTS
-    }
-];
+  {
+    // START
+    name: 'start',
+    type: START,
+  },
+  {
+    // Question one
+    type: QUESTION_TYPE_MULTIPLE,
+    question: 'Which of these fruits do you prefer?',
+    choices: [
+      {
+        // Choice two
+        answer: 'Apples, raspberries, or strawberries',
+        result: RESULT_BLUE,
+        add: 10,
+      },
+      {
+        // Choice one
+        answer: 'Blueberries',
+        result: RESULT_RED,
+        add: 1,
+      },
+      {
+        // Choice three
+        answer: 'Bananas',
+        result: RESULT_YELLOW,
+        add: 1,
+      },
+      {
+        // Choice four
+        answer: 'Grapes',
+        result: RESULT_GREEN,
+        add: 1,
+      },
+      {
+        // Choice five
+        answer: 'Blackberries',
+        result: RESULT_BLACK,
+        add: 1,
+      },
+    ],
+  },
+  {
+    // Question two, this question doesn't influence the results at all.
+    type: QUESTION_TYPE_MULTIPLE,
+    question: 'What is your name?',
+    choices: [
+      {
+        // Choice two
+        answer: 'Denise',
+        result: 0,
+        add: 0,
+      },
+      {
+        // Choice one
+        answer: 'Josh',
+        result: 0,
+        add: 0,
+      },
+      {
+        // Choice three
+        answer: 'Not listed',
+        result: 0,
+        add: 0,
+        input: true
+      },
+    ],
+  },
+  {
+    type: QUESTION_TYPE_MULTIPLE,
+    question: 'If you were gifted a new car, what color would you paint it?',
+    choices: [
+      {
+        answer: 'Candy apple red',
+        result: RESULT_BLUE,
+        add: 10,
+      },
+      {
+        answer: 'Light blue sky',
+        result: RESULT_RED,
+        add: 1,
+      },
+      {
+        answer: 'Rainbow confetti',
+        result: RESULT_YELLOW,
+        add: 1,
+      },
+      {
+        answer: 'Iridescent',
+        result: RESULT_GREEN,
+        add: 1,
+      },
+      {
+        answer: 'Shadow gothic dark sadness matte black',
+        result: RESULT_BLACK,
+        add: 1,
+      },
+    ],
+  },
 
+  {
+    type: QUESTION_TYPE_MULTIPLE,
+    question: 'How is your day?',
+    choices: [
+      {
+        // Choice one
+        answer: 'Awesome',
+        result: RESULT_RED,
+        add: 1,
+      },
+      {
+        // Choice two
+        answer: 'Good',
+        result: RESULT_BLUE,
+        add: 1,
+      },
+      {
+        // Choice three
+        answer: 'Decent',
+        result: RESULT_YELLOW,
+        add: 1,
+      },
+      {
+        // Choice four
+        answer: 'Amiss',
+        result: RESULT_GREEN,
+        add: 1,
+      },
+      {
+        // Choice five
+        answer: 'Bad',
+        result: RESULT_BLACK,
+        add: 1,
+      },
+    ],
+  },
+  {
+    type: QUESTION_TYPE_MULTIPLE,
+    question: 'Pick a color that describes you: ',
+    choices: [
+      {
+        // Choice one
+        answer: 'Red',
+        result: RESULT_BLUE,
+        color: '#dc1717',
+        add: 10,
+      },
+      {
+        // Choice two
+        answer: 'Blue',
+        result: RESULT_RED,
+        color: '#0041ff',
+        add: 1,
+      },
+      {
+        // Choice three
+        answer: 'Yellow',
+        result: RESULT_YELLOW,
+        color: '#F3E13D',
+        add: 1,
+      },
+      {
+        // Choice four
+        answer: 'Magenta',
+        result: RESULT_GREEN,
+        color: '#e432ff',
+        add: 1,
+      },
+      {
+        // Choice five
+        answer: 'Purple',
+        result: RESULT_BLACK,
+        color: '#6c02ed',
+        add: 1,
+      },
+    ],
+  },
+  {
+    // Results
+    name: 'results',
+    type: RESULTS,
+  },
+];
 
 export default DATA;
